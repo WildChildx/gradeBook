3ebc480645bb4e8cf086b6a1739f37de8db8fcdc
Shree Chatane <58407535+WildChildx@users.noreply.github.com>
2024-06-30 14:35:39 +0530

    adding unit test project

---
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace GradeBook.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var x = 5;
+        var y = 2;
+        var actual = x * y;
+        var expected = 7;
+
+        Assert.Equal(expected, actual);
+    }
+}
