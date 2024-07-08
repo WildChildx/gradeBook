6466b19ee6003e97123b10ef39e32e6ab01c9e22
Shree Chatane <58407535+WildChildx@users.noreply.github.com>
2024-07-08 21:24:20 +0530

    update gitignore 3

--- src/GradeBook/obj/Debug/net8.0/GradeBook.AssemblyInfo.cs
+++ src/GradeBook/obj/Debug/net8.0/GradeBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3dcd406878a2d579037c716d1abf8a95d1a2ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b2da63184a307145ba7f30e6665f530509b4e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
